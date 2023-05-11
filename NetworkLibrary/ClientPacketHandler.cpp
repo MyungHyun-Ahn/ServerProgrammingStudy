73e6566ded6c5764e2ff07aa2dde40d8963923d7
MyungHyun-Ahn <ahn020367@gmail.com>
2023-05-11 23:15:52 +0900

    패킷 직렬화 * Send 부분에서 임시객체 사용 X 버퍼 그대로 갖다쓰는 방법 구현

--- NetworkLibrary/ClientPacketHandler.cpp
+++ NetworkLibrary/ClientPacketHandler.cpp
@@ -5,10 +5,6 @@
 
 // S_ : 서버에서 클라로 보내는 경우
 // C_ : 클라에서 서버로 보내는 경우
-enum
-{
-	S_TEST = 1,
-};
 
 void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 {
@@ -32,7 +28,7 @@ void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 
 #pragma pack(1)
 // 패킷 설계 TEMP
-// [ PKT_S_TEST ] [ BuffData BuffData BuffData ] [      ] [        ]
+// [ PKT_S_TEST ] [ BuffListItem BuffListItem BuffListItem ] [      ] [        ]
 struct PKT_S_TEST
 {
 	struct BuffListItem
@@ -68,6 +64,11 @@ struct PKT_S_TEST
 
 		// 패킷의 크기를 구하기
 		size += sizeof(PKT_S_TEST);
+
+		// 밖에서 하던 len 체크를 여기서 수행
+		if (packetSize < size)
+			return false;
+
 		size += buffsCount * sizeof(BuffListItem);
 		if (size != packetSize)
 			return false;
@@ -77,20 +78,43 @@ struct PKT_S_TEST
 	}
 	// vector<BuffData> buffs;
 	// wstring name;
+
+	using BuffsList = PacketList<PKT_S_TEST::BuffListItem>;
+
+	BuffsList GetBuffsList()
+	{
+		BYTE* data = reinterpret_cast<BYTE*>(this);
+		data += buffsOffset;
+		return BuffsList(reinterpret_cast<PKT_S_TEST::BuffListItem*>(data), buffsCount);
+	}
 };
 #pragma pack()
 
+// [ PKT_S_TEST ] [ BuffListItem BuffListItem BuffListItem ]
 void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 {
 	BufferReader br(buffer, len);
 
+	/*
+	// 꺼내쓰지 않고 그냥 내용을 가져다 쓰는 것을 길이 체크를 하지 않아도 됨
 	if (len < sizeof(PKT_S_TEST))
 		return;
+	*/
 
-	PKT_S_TEST pkt;
-	br >> pkt;
+	// 꺼내쓰지 않고 buffer를 그냥 씀
+	// 임시 객체를 만들어서 복사한 것이 아닌 그냥 buffer의 주소에 있는 내용을 곧바로 가져다 씀
+	// -> 복사 비용을 한 번 아낄 수 있다.
+	PKT_S_TEST* pkt = reinterpret_cast<PKT_S_TEST*>(buffer);
 
-	if (pkt.Validate() == false)
+	// 4바이트 (헤더) 까지는 정상적으로 왔지만
+	// 그 이후의 데이터는 모름
+	// PKT_S_TEST::Validate 함수에서 체크
+
+	// PKT_S_TEST pkt;
+	// br >> pkt;
+
+	// 헤더 이후부터 안전하게 사용할 수 있는지 체크
+	if (pkt->Validate() == false)
 		return;
 
 	// 불편한 점 : 보낸 순서와 받은 순서를 맞춰 주어야 함
@@ -99,22 +123,30 @@ void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 	// 실제 라이브 서비스 서버 : 100~
 	// cout << "ID : " << id << " HP : " << hp << " ATT : " << attack << endl;
 
-	vector<PKT_S_TEST::BuffListItem> buffs;
+	// vector<PKT_S_TEST::BuffListItem> buffs;
 
 
 	// buffCount를 신뢰할 수 없음
 	// 나중에는 이것을 체크할 방법을 구현해야함
 
-	buffs.resize(pkt.buffsCount);
-	for (int32 i = 0; i < pkt.buffsCount; i++)
-	{
-		br >> buffs[i];
-	}
+	PKT_S_TEST::BuffsList buffs = pkt->GetBuffsList();
 
-	cout << "BuffCount : " << pkt.buffsCount << endl;
-	for (int32 i = 0; i < pkt.buffsCount; i++)
+	cout << "BuffCount : " << buffs.Count() << endl;
+	for (int32 i = 0; i < buffs.Count(); i++)
 	{
 		cout << "BuffInfo : " << buffs[i].buffId << " " << buffs[i].remainTime << endl;
+	}
+
+	// iterator
+	for (auto it = buffs.begin(); it != buffs.end(); ++it)
+	{
+		cout << "Iterator BuffInfo : " << it->buffId << " " << it->remainTime << endl;
+	}
+
+	// ranged-base
+	for (auto& buff : buffs)
+	{
+		cout << "Ranged-Base BuffInfo : " << buff.buffId << " " << buff.remainTime << endl;
 	}
 
 	// 문자열은 나중에 처리
--- NetworkLibrary/ClientPacketHandler.h
+++ NetworkLibrary/ClientPacketHandler.h
@@ -1,5 +1,10 @@
 #pragma once
 
+enum
+{
+	S_TEST = 1,
+};
+
 /*-----------------------
    ClientPacketHandler
 -----------------------*/
@@ -10,5 +15,51 @@ public:
 	static void HandlePacket(BYTE* buffer, int32 len);
 
 	static void Handle_S_TEST(BYTE* buffer, int32 len);
+};
+
+template<typename T, typename C>
+class PacketIterator
+{
+public:
+	PacketIterator(C& container, uint16 index) : _container(container), _index(index) { }
+
+	bool				operator!=(const PacketIterator& other) const { return _index != other._index; }
+	const T&			operator*() const { return _container[_index]; }
+	T&					operator*() { return _container[_index]; }
+	T*					operator->() { return &_container[_index]; }
+	PacketIterator&		operator++() { _index++; return *this; }
+	PacketIterator		operator++(int32) { PacketIterator ret = *this; ++_index; return ret; }
+
+
+private:
+	C&			_container;
+	uint16		_index;
+};
+
+template<typename T>
+class PacketList
+{
+public:
+	PacketList() : _data(nullptr), _count(0) { }
+	PacketList(T* data, uint16 count) : _data(data), _count(count) { }
+
+	T& operator[](uint16 index)
+	{
+		// 오버플로우 체크
+		ASSERT_CRASH(index < _count);
+		return _data[index];
+	}
+
+	uint16 Count() { return _count; }
+
+	// ranged-base 지원
+	PacketIterator<T, PacketList<T>> begin() { return PacketIterator<T, PacketList<T>>(*this, 0); }
+	PacketIterator<T, PacketList<T>> end() { return PacketIterator<T, PacketList<T>>(*this, _count); }
+
+
+
+private:
+	T*       _data;    // 시작 주소
+	uint16   _count;   // 데이터 개수
 };
 
